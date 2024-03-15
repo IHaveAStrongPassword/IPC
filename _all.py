a62e8b50c862d43ccd8b8c74e01e2d9eeec5a37b
IHaveAStrongPassword <alexmchen0@gmail.com>
2024-03-15 11:27:02 -0700

    Update _all.py

--- _all.py
+++ _all.py
@@ -1,3 +1,11 @@
+"""
+Adapted from batch.py
+
+- Implemented pause functionality so that incomplete models resume on next run.
+"""
+
+THREADS = 12
+
 import subprocess
 import os
 from multiprocessing import Pool
@@ -7,14 +15,12 @@ import re
 import argparse
 import glob
 
-THREADS = 12
-
 cwd = os.path.realpath('.')
 input = f'{cwd}/input/'
 output = f'{cwd}/output/'
 incomplete = f'{cwd}/incomplete/'
 
-parser = argparse.ArgumentParser(description='Run models in IPC')
+parser = argparse.ArgumentParser(description='Run models in IPC with pause functionality')
 parser.add_argument('-p', '--print', action='store_true', help='Prints commands only')
 args = parser.parse_args()
 
@@ -34,48 +40,46 @@ NTSetStr1 = 'export OMP_NUM_THREADS='
 # for Mac when CHOLMOD is compiled with default LAPACK and BLAS
 NTSetStr2 = 'export VECLIB_MAXIMUM_THREADS='
 
-setThreadCount = f'{NTSetStr0}{THREADS}\n{NTSetStr1}{THREADS}\n{NTSetStr2}{THREADS}\n'
 
+setThreadCount = f'{NTSetStr0}{THREADS}\n{NTSetStr1}{THREADS}\n{NTSetStr2}{THREADS}\n'
 if args.print:
     print(setThreadCount)
-
-def atoi(text):
-    return int(text) if text.isdigit() else text
-def natural_keys(text):
-    '''
-    alist.sort(key=natural_keys) sorts in human order
-    http://nedbatchelder.com/blog/200712/human_sorting.html
-    (See Toothy's implementation in the comments)
-    '''
-    return [ atoi(c) for c in re.split(r'(\d+)', text) ]
-
-def run(model):
-    inp = f'{input}{model}'
-    s = glob.glob(f'{incomplete}{model}/status*')
-    s.sort(key=natural_keys)
-    if len(s) > 0:
-        s = s[-1]
-        text = ''
-        with open(inp, 'r') as f:
-            text = f.read()
-        prev = t.find('\nrestart')
-        if prev > -1:
-            text = text[:prev]
-        text = f'{t}\nrestart {s}\n'
-        with open(i, 'w') as f:
-            f.write(text)
-
-    inc = f'incomplete/{model}'
-    out = f'output/{model}'
-    
-    cmd = f'{progPath} 100 {inp} --numThreads {THREADS} -o {inc}'
-    if args.print:
-        print(cmd)
-        return
-    subprocess.call([setThreadCount + cmd], shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    print(f'complete: {model}')
-    subprocess.call([f'mv {inc} {out}'], shell=True)
 if True:
+    def atoi(text): return int(text) if text.isdigit() else text
+    def natural_keys(text):
+        '''
+        alist.sort(key=natural_keys) sorts in human order
+        http://nedbatchelder.com/blog/200712/human_sorting.html
+        (See Toothy's implementation in the comments)
+        '''
+        return [ atoi(c) for c in re.split(r'(\d+)', text) ]
+    def run(model):
+        inp = f'input/{model}'
+        status = glob.glob(f'{incomplete}{model}/status*')
+        status.sort(key=natural_keys)
+        if len(status) > 0:
+            status = status[-1]
+            text = ''
+            with open(inp, 'r') as f:
+                text = f.read()
+            end = t.find('\nrestart')
+            if end > -1:
+                text = text[:end]
+            text = f'{text}\nrestart {status}\n'
+            with open(inp, 'w') as f:
+                f.write(text)
+    
+        inc = f'incomplete/{model}'
+        out = f'output/{model}'
+        
+        if args.print:
+            cmd = f'{progPath} 100 {inp} --numThreads {THREADS} -o {out}'
+            print(cmd)
+            return
+        cmd = f'{progPath} 100 {inp} --numThreads {THREADS} -o {inc}'
+        subprocess.call([setThreadCount + cmd], shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        print(f'complete: {model}')
+        subprocess.call([f'mv {inc} {out}'], shell=True)
     def shouldRun(f):
         hasOutput =  isdir(join(output, f))
         #hasIncomplete = isdir(join(incomplete, f))
